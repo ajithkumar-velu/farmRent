d9d5061d392654aaed19acf03c367fee5419a364
aji <ajithajk46@gmail.com>
2024-10-22 11:45:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,10 @@ app.use('/api/provider', providerRouter)
 app.use('/api/order', orderRoute)
 
 app.get('/', (req, res)=>{
-<<<<<<< HEAD
-=======
+
+
     console.log("hi")
->>>>>>> 046755e9f54cf077c07c56559394e2fff5b1d5a2
+
     
     res.send("API is working")
 })
