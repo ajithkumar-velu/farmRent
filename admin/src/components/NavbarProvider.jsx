6944192f7c3a682cb95d60a7d4f583732d2f1ae1
aji <ajithajk46@gmail.com>
2024-10-22 13:58:29 +0530

    Update NavbarProvider.jsx

--- admin/src/components/NavbarProvider.jsx
+++ admin/src/components/NavbarProvider.jsx
@@ -14,7 +14,6 @@ const NavbarProvider = () => {
     <div className='flex items-center justify-between py-5 px-[4%]' >
       <p onClick={(() => navigate('/'))} className="text-4xl text-green-400 font-bold cursor-pointer" >FarmRent</p>
       <div className='flex items-center gap-3' >
-        <a href='https://farm-rent-frontend.vercel.app/' className='border px-5 py-1 rounded-full border-black' >Former</a>
         <button onClick={logout} className='px-5 py-2 sm:px-6 text-xs sm:text-sm bg-gray-700 text-white rounded-full' >Logout</button>
       </div>
     </div>
